90543629286a909eda3d02b76b0a8534041881d1
Satyam Tomar <143343967+SatyamStellar@users.noreply.github.com>
2023-10-22 23:50:54 +0530

    A new random password generator

---
+++ Minor Projects/Random Password 2/index.js
@@ -0,0 +1,42 @@
+let rangeNum = document.querySelector(".range");
+let options = document.querySelectorAll(".option input");
+let copyBtn = document.querySelector(".copy-btn");
+let inpPass = document.querySelector(".input-pass");
+let genbtn = document.querySelector(".gen-btn");
+
+const characters = {
+    uppercases: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercases: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "0123456789",
+    symbols: "~!@#$%^&*()_+{}:[];,."
+}
+
+const genPass = () => {
+    let staticPass = "";
+    let ranPass = "";
+    let passLength = rangeNum.value;
+
+    options.forEach(option => {
+        if(option.checked) {
+            staticPass += characters[option.id];
+        }
+    });
+
+    for (let i = 0; i < passLength; i++) {
+        ranPass += staticPass[Math.floor(Math.random() * staticPass.length)];
+    }
+    inpPass.value = ranPass;
+}
+
+const updateSlider = () => {
+    document.querySelector(".rang-value").innerHTML = rangeNum.value;
+}
+
+const copyPass = () => {
+    navigator.clipboard.writeText(inpPass.value);
+    window.alert("Text copied");
+}
+
+copyBtn.addEventListener("click", copyPass);
+rangeNum.addEventListener("input", updateSlider);
+genbtn.addEventListener("click", genPass);
