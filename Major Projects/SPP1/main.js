c1c11e38f693acc7cb3a0d2066c4eb1bc1509204
Satyam Tomar <satyam321tomar@gmail.com>
2024-09-19 21:41:21 +0530

    Progress in SPP1

--- Major Projects/SPP1/main.js
+++ Major Projects/SPP1/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+gsap.to(".vid_div", {
+  scrollTrigger: {
+    trigger: ".home",
+    start: "top top",
+    end: "bottom top",
+    scrub: 2,
+    pin: true,
+  },
+  '--clip': "0%",
+  ease: Power2,
+  duration: 2,
+})
