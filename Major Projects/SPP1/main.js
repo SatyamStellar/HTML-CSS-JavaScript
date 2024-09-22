8f98ff348805fe489b8bfbb16dc78aa244e68138
Satyam Tomar <satyam321tomar@gmail.com>
2024-09-23 00:06:39 +0530

    adding animation to the spp1 project

--- Major Projects/SPP1/main.js
+++ Major Projects/SPP1/main.js
@@ -1,12 +1,53 @@
-gsap.to(".vid_div", {
-  scrollTrigger: {
-    trigger: ".home",
-    start: "top top",
-    end: "bottom top",
-    scrub: 2,
-    pin: true,
-  },
-  '--clip': "0%",
-  ease: Power2,
-  duration: 2,
-})
+function homePageAnimation() {
+  gsap.set(".slidem", {
+    scale: 5
+  })
+
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 2,
+    }
+  });
+
+  tl
+    .to(".vid_div", {
+      '--clip': "0%",
+      ease: Power2,
+    }, 'a')
+    .to(".slidem", {
+      scale: 1,
+      ease: Power2
+    }, 'a')
+    .to(".lft", {
+      xPercent: -10,
+      ease: Power2,
+      stagger: .03
+    }, 'b')
+    .to(".rgt", {
+      xPercent: 10,
+      ease: Power2,
+      stagger: .03
+    }, 'b')
+}
+
+function realPageAnimation() {
+  gsap.to(".slide", {
+    scrollTrigger: {
+      trigger: ".real",
+      start: "top top",
+      end: "bottom bottom",
+      markers: true,
+      scrub: 1.5
+    },
+    x: "-200%",
+    ease: Power4
+  })
+}
+
+
+
+homePageAnimation();
+realPageAnimation();
