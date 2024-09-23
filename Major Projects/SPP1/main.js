33d1a5bfc1bf55b40a284417d67cca7a67517b30
Satyam Tomar <satyam321tomar@gmail.com>
2024-09-23 13:34:08 +0530

    complete the spp1

--- Major Projects/SPP1/main.js
+++ Major Projects/SPP1/main.js
@@ -39,7 +39,6 @@ function realPageAnimation() {
       trigger: ".real",
       start: "top top",
       end: "bottom bottom",
-      markers: true,
       scrub: 1.5
     },
     x: "-200%",
@@ -47,7 +46,83 @@ function realPageAnimation() {
   })
 }
 
+function teamPageAnimation() {
+  document.querySelectorAll(".list_items")
+    .forEach((elem) => {
+      elem.addEventListener("mousemove", (dets) => {
+
+        gsap.to(elem.querySelector(".picture"), {
+          opacity: 1,
+          x: gsap.utils.mapRange(0, innerWidth, -200, 200, dets.clientX),
+          ease: Power4,
+        })
+      })
+      elem.addEventListener("mouseleave", (dets) => {
+        gsap.to(elem.querySelector(".picture"), {
+          opacity: 0,
+          ease: Power4,
+
+        })
+      })
+    })
+
+}
+
+function paraPageAnimation() {
+
+  let clutter = "";
+  document.querySelector(".textpara")
+    .textContent.split("")
+    .forEach((e) => {
+      if (e === " ") clutter += `<span>&nbsp;</span>`
+      clutter += `<span>${e}</span>`
+    })
+  document.querySelector(".textpara").innerHTML = clutter
+
+  gsap.set(".textpara span", {
+    opacity: .1,
+  })
+
+  gsap.to(".textpara span", {
+    scrollTrigger: {
+      trigger: ".paras",
+      start: "top 70%",
+      end: "bottom 80%",
+      scrub: 2,
+    },
+    opacity: 1,
+    stagger: 0.1,
+    ease: Power4
+  })
+
+}
+
+function loco() {
+
+  (function () {
+    const locomotiveScroll = new LocomotiveScroll();
+  })();
+}
 
 
+document.querySelectorAll(".section").forEach((e) => {
+  ScrollTrigger.create({
+    trigger: e,
+    start: "top 50%",
+    end: "bottom 50%",
+    markers: true,
+    onEnter: () => {
+      document.body.setAttribute("theme", e.dataset.col)
+    },
+    onEnterBack: () => {
+      document.body.setAttribute("theme", e.dataset.col)
+    }
+  })
+})
+
+
+loco();
 homePageAnimation();
 realPageAnimation();
+teamPageAnimation();
+paraPageAnimation();
