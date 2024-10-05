840a2075834d75ef1be6efd1176305cbd8929b06
Satyam Tomar <satyam321tomar@gmail.com>
2024-10-05 23:29:12 +0530

    Complete the about sec of obys agency

--- Major Projects/Obsy Clone/main.js
+++ Major Projects/Obsy Clone/main.js
@@ -34,8 +34,6 @@ function locomotiveAnimation() {
 
 }
 
-
-
 document.addEventListener("mousemove", (dets) => {
   gsap.to(".cursor", {
     top: dets.y,
@@ -102,8 +100,6 @@ function loaderAnimation() {
     })
 }
 
-
-
 function magnateEffect() {
 
   const buttons = document.querySelectorAll('nav a, .menu-opener__square ');
@@ -140,17 +136,20 @@ function magnateEffect() {
   })
 }
 
+function sheryAnimation() {
+
+  Shery.imageEffect(".img__div", {
+    style: 5,
+    config: { "a": { "value": 1.37, "range": [0, 30] }, "b": { "value": 0.75, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272787057740365 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.15, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.6, "range": [0, 2] }, "discard_threshold": { "value": 0.73, "range": [0, 1] }, "antialias_threshold": { "value": 0.01, "range": [0, 0.1] }, "noise_height": { "value": 0.55, "range": [0, 2] }, "noise_scale": { "value": 7.63, "range": [0, 100] } },
+    gooey: true,
+  });
+}
 
 
 // loaderAnimation();
 locomotiveAnimation();
 magnateEffect();
+sheryAnimation();
 
 
 
-Shery.imageEffect(".img__div", {
-  style: 5,
-  debug: false,
-  config: { "a": { "value": 1.37, "range": [0, 30] }, "b": { "value": 0.75, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272787057740365 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.15, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.6, "range": [0, 2] }, "discard_threshold": { "value": 0.73, "range": [0, 1] }, "antialias_threshold": { "value": 0.01, "range": [0, 0.1] }, "noise_height": { "value": 0.55, "range": [0, 2] }, "noise_scale": { "value": 7.63, "range": [0, 100] } },
-  gooey: true,
-});
