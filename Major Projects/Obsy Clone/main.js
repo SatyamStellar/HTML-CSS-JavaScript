3aca0f7f27953770c5b14524fb4d77f140122150
Satyam Tomar <satyam321tomar@gmail.com>
2024-10-05 22:37:03 +0530

    Finaly resolve the shery.js problem

--- Major Projects/Obsy Clone/main.js
+++ Major Projects/Obsy Clone/main.js
@@ -1,11 +1,4 @@
 
-Shery.imageEffect(".img__div", {
-  style: 5,
-  debug: true,
-  gooey: true,
-});
-
-
 
 
 function locomotiveAnimation() {
@@ -155,4 +148,9 @@ magnateEffect();
 
 
 
-
+Shery.imageEffect(".img__div", {
+  style: 5,
+  debug: false,
+  config: { "a": { "value": 1.37, "range": [0, 30] }, "b": { "value": 0.75, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272787057740365 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.15, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.6, "range": [0, 2] }, "discard_threshold": { "value": 0.73, "range": [0, 1] }, "antialias_threshold": { "value": 0.01, "range": [0, 0.1] }, "noise_height": { "value": 0.55, "range": [0, 2] }, "noise_scale": { "value": 7.63, "range": [0, 100] } },
+  gooey: true,
+});
