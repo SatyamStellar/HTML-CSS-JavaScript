3a01abbf4583db7c1d75daf75e8405952b4814b4
Satyam Tomar <satyam321tomar@gmail.com>
2024-10-07 15:06:09 +0530

    Complet the projects section in obys clone

--- Major Projects/Obsy Clone/main.js
+++ Major Projects/Obsy Clone/main.js
@@ -1,45 +1,3 @@
-
-
-
-function locomotiveAnimation() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".main"),
-    smooth: true
-  });
-
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-
-  ScrollTrigger.scrollerProxy(".main", {
-    scrollTop(value) {
-      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-
-    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-  });
-
-
-
-
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-
-}
-
-document.addEventListener("mousemove", (dets) => {
-  gsap.to(".cursor", {
-    top: dets.y,
-    left: dets.x,
-  })
-})
 function loaderAnimation() {
 
   gsap.registerPlugin(ScrollTrigger);
@@ -100,6 +58,50 @@ function loaderAnimation() {
     })
 }
 
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
+
+
+
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+
+}
+
+
+function cursorAnimation() {
+  Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1
+  })
+}
+
+
+
 function magnateEffect() {
 
   const buttons = document.querySelectorAll('nav a, .menu-opener__square ');
@@ -150,6 +152,4 @@ function sheryAnimation() {
 locomotiveAnimation();
 magnateEffect();
 sheryAnimation();
-
-
-
+cursorAnimation();
